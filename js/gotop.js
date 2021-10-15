1290cb5f3fc83f8fb997d107a5c9da616d4749dd
kjarango <kjarango64559@umanizales.edu.co>
2021-10-15 17:08:17 -0500

    modificacion de la galeria

--- js/gotop.js
+++ js/gotop.js
@@ -1,18 +1,4 @@
-window.onscroll= function(){
-    console.log(document.documentElement.scrolltop)
-    if(document.documentElement.scrolltop > 100){
-        document.querySelector('.go-top-container').classList.add('show');
-    }else{
-        document.querySelector('.go-top-container').classList.remove('show')
-    }
-}
 
-document.querySelector('.go-top-container').addEventListener('click', ()=> {
-    window.scrollTo({
-        top:0,
-        behavior: 'smooth'
-    })
-})
 
 let caja=document.getElementById("subir__arriba");
 caja.addEventListener("click",function(){
